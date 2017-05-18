62e46e9cbe56a15b1d58a0b635ef6b2dbf44942f
chrisonntag <sonntagc@fim.uni-passau.de>
2017-05-18 11:01:54 +0200

    Update requirements, gitignore and setup basic cli interface

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,25 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import, print_function
 import click
 
 
-@click.command()
+def pr_mmetering_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('mmetering v1.1')
+    ctx.exit()
+
+@click.group()
+@click.version_option(help='Show mmetering-cli version')
 def main():
-    print('I am mmetering-cli')
+    """mmetering-cli"""
+    nothing = None
+
+@main.command()
+@click.option('-n', '--name',  help='Prints your name')
+def sync(name):
+    click.echo("%s" % name)
+
+@main.command()
+@click.option('--version', is_flag=True, callback=pr_mmetering_version, 
+        expose_value=False, is_eager=True, help='Show mmetering version')
+def mmetering():
+    nothing = None
